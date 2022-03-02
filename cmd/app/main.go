3d38321fb03099c10865025d3ddf47b395ede405
Vai3soh <he9tei7@protonmail.com>
2022-03-02 11:28:57 +1000

    Refactoring entrypoint. Change logic scraping urls company

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"log"
+	"os"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/Vai3soh/speedtestVps/pkg/wget-go"
 	"github.com/gocolly/colly/v2"
 )
 
-type Country string
 type HostingLink string
-type City string
-type Location string
 
-type Data struct {
-	Hosting map[HostingLink]map[Location][]network
-}
+var percentLimit string
+var downloadFileSize int
 
 type network struct {
 	portSpeed    string
 	linkDownload string
+	speedTest    float64
+}
+
+type SaveData struct {
+	HostingLink
+	DataCenterAndSpeedTestLink []map[string][]network
+}
+
+func init() {
+
+	flag.StringVar(&percentLimit, "L", "100", "limit download %")
+	flag.IntVar(&downloadFileSize, "D", 10, "download file size {10,100,1000 MB} %")
+	flag.Parse()
 }
 
 func removeAndModify(items []string, item string) []string {
@@ -36,22 +49,56 @@ func removeAndModify(items []string, item string) []string {
 	return newitems
 }
 
-var percentLimit string
+func downloadOrNot(link string) bool {
+	switch downloadFileSize {
+	case 10:
+		if strings.Contains(link, "10.mb") {
+			return true
+		}
+	case 100:
+		if strings.Contains(link, "100.mb") {
+			return true
+		}
+	case 1000:
+		if strings.Contains(link, "1000.mb") {
+			return true
+		}
+	}
+	return false
+}
 
-func init() {
+func speedTest(linkDownload string) ([]float64, error) {
+	spd, _, err := wget.WgetCli([]string{"", "-O", "/dev/null", "-L", percentLimit, linkDownload})
+	if err != nil {
+		return nil, err
+	}
+	return spd, err
+}
 
-	flag.StringVar(&percentLimit, "L", "100", "limit download %")
-	flag.Parse()
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
+}
+
+func saveToCsv(data [][]string) {
+	file, err := os.Create("result.csv")
+	checkError("Cannot create file", err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.WriteAll(data)
+	checkError("Cannot write to file", err)
 }
 
 func main() {
 
 	c := colly.NewCollector()
 
-	data := &Data{}
-	networkTest := make([]network, 0)
-	locationTests := make(map[Location][]network)
-	data.Hosting = make(map[HostingLink]map[Location][]network)
+	s := &SaveData{}
+	all := []SaveData{}
 
 	detail := c.Clone()
 
@@ -73,42 +120,88 @@ func main() {
 
 	detail.OnHTML("body", func(e *colly.HTMLElement) {
 
-		hostingLocations := e.ChildTexts("button.btn.btn-default.btn-block")
-		hostingLocations = removeAndModify(hostingLocations, "Full info")
-		data.Hosting[HostingLink(e.ChildText("span.pull-right"))] = locationTests
+		hostingLink := e.ChildText("span.pull-right")
+		dataCenters := e.ChildTexts("button.btn.btn-default.btn-block")
+		dataCenters = removeAndModify(dataCenters, "Full info")
+		linkDownloads := e.ChildAttrs("div.btn-group.btn-group-justified a.btn.btn-default", "href")
+		titles := e.ChildAttrs("div.btn-group.btn-group-justified a.btn.btn-default", "title")
 
-		count := 0
-		e.ForEach("div.btn-group.btn-group-justified a", func(_ int, elem *colly.HTMLElement) {
-			count++
-			n := network{portSpeed: elem.Attr(`title`), linkDownload: elem.Attr(`href`)}
-			networkTest = append(networkTest, n)
+		merge := [][]network{}
+		group := []network{}
 
-			for i, hostingLocation := range hostingLocations {
-				if hostingLocation != "" {
-					locationTests[Location(hostingLocation)] = networkTest
-					if count == 3 {
-						hostingLocations[i] = ""
-						count = 0
-						networkTest = []network{}
-						continue
+		for i := range linkDownloads {
+
+			spd := 0.0
+			ok := downloadOrNot(linkDownloads[i])
+			if ok {
+				spds, err := speedTest(linkDownloads[i])
+				if err != nil {
+					log.Println()
+					log.Println(err)
+				} else {
+					if len(spds) != 0 {
+						spd = spds[0]
 					}
-					break
 				}
 			}
-		})
+			nw := &network{
+				portSpeed:    titles[i],
+				linkDownload: linkDownloads[i],
+				speedTest:    spd,
+			}
+
+			group = append(group, *nw)
+
+			if len(group) == 3 {
+				merge = append(merge, group)
+				group = []network{}
+			}
+		}
+
+		s.HostingLink = HostingLink(hostingLink)
+		sliceMaps := []map[string][]network{}
+		if len(dataCenters) == len(merge) {
+			for i := range dataCenters {
+				sliceMaps = append(sliceMaps, map[string][]network{
+					dataCenters[i]: merge[i],
+				})
+			}
+		}
+		s.DataCenterAndSpeedTestLink = sliceMaps
+		all = append(all, *s)
 	})
 
 	c.Visit("https://looking.house/points.php")
+	writer := [][]string{}
+	//writer = append(writer, []string{"HostingLink", "DataCentr", "LinkDownload", "SpeedTest Mbit/s"})
 
-	for _, value := range data.Hosting {
-		for location, networkLink := range value {
-			spd, _, err := wget.WgetCli([]string{"", "-O", "/dev/null", "-L", percentLimit, networkLink[0].linkDownload})
-			if err != nil {
-				log.Println()
-				log.Println(err)
-				continue
+	writer = [][]string{}
+	for _, e := range all {
+		for _, v := range e.DataCenterAndSpeedTestLink {
+			for name, ex := range v {
+				for _, vl := range ex {
+					if vl.speedTest != 0 {
+						writer = append(writer,
+							[]string{
+								string(e.HostingLink),
+								name,
+								vl.linkDownload,
+								strconv.FormatFloat(vl.speedTest, 'f', 2, 64)},
+						)
+					}
+				}
 			}
-			log.Printf("\nLocation: %s, port speed: %s, link: %s, speedtest: %0.2fMbit/s", location, networkLink[0].portSpeed, networkLink[0].linkDownload, spd[0])
 		}
 	}
+
+	sort.Slice(writer, func(i, j int) bool {
+		for x := 0; x < len(writer[i]); x++ {
+			v1, _ := strconv.ParseFloat(writer[i][3], 64)
+			v2, _ := strconv.ParseFloat(writer[j][3], 64)
+			return v1 > v2
+		}
+		return false
+	})
+	saveToCsv(writer)
+
 }
